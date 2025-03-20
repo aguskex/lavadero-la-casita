bf23bafcbe72168279b7beedfb498d1c67ad06df
aguskex <109179354+aguskex@users.noreply.github.com>
2025-03-20 17:32:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-// scroll-to-top.js
-
 document.addEventListener("DOMContentLoaded", function() {
     const scrollToTopButton = document.querySelector('.scroll-to-top');
   
-    // Mostrar u ocultar el botón basado en el scroll
     window.addEventListener('scroll', () => {
       if (window.scrollY > 300) {
         scrollToTopButton.style.display = 'flex';
@@ -11,8 +8,6 @@ document.addEventListener("DOMContentLoaded", function() {
         scrollToTopButton.style.display = 'none';
       }
     });
-  
-    // Scroll suave al hacer clic en el botón
     scrollToTopButton.addEventListener('click', () => {
       window.scrollTo({
         top: 0,
